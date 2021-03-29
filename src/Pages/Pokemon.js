cc356c3f7f4895c4e8c0500742cc3a9f1d8dd191
Erick Vega <erikvega30@hotmail.com>
2021-03-29 03:06:06 -0600

    CSS updated

--- src/Pages/Pokemon.js
+++ src/Pages/Pokemon.js
@@ -3,6 +3,27 @@ import { useSelector, useDispatch } from 'react-redux'
 import { GetPokemon } from '../actions/pokemonActions'
 import _ from 'lodash'
 
+const TYPE_COLORS = {
+    bug: 'B1C12E',
+    dark: '4F3A2D',
+    dragon: '755EDF',
+    electric: 'FCBC17',
+    fairy: 'F4B1F4',
+    fighting: '823551D',
+    fire: 'E73B0C',
+    flying: 'A3B3F7',
+    ghost: '6060B2',
+    grass: '74C236',
+    ground: 'D3B357',
+    ice: 'A3E7FD',
+    normal: 'C8C4BC',
+    poison: '934594',
+    psychic: 'ED4882',
+    rock: 'B9A156',
+    steel: 'B5B5C3',
+    water: '3295F6'
+  };
+
 const Pokemon = (props) => {
     const pokemonName = props.match.params.pokemon
     const dispatch = useDispatch()
@@ -16,7 +37,15 @@ const Pokemon = (props) => {
             const pokeData = pokemonState.data[pokemonName]
             return(
                 <section className='glass'>
+                    <div className='header-poke'>
                     <h2 className='name'>{pokeData.name}</h2>
+                    <span className='number'>N.° {pokeData.id}</span>
+                    </div>
+                    <div className="type">
+                        {pokeData.types.map(type => (
+                            <span key={type.type.name} style={{backgroundColor: `#${TYPE_COLORS[type.type.name]}`, color: '#ffffff', padding: '10px 15px', borderRadius: '20px'}}>{type.type.name}</span>
+                        ))}
+                    </div>
                     <div className='pictures'>
                         <div className='row'>
                             <img src={pokeData.sprites.front_default} alt="imgFront"/>
@@ -25,6 +54,29 @@ const Pokemon = (props) => {
                         <div className='row'>
                             <img src={pokeData.sprites.front_shiny} alt="imgBack"/>
                             <img src={pokeData.sprites.back_shiny} alt="imgShinyB"/>
+                        </div>
+                    </div>
+                    <div className='description'>
+                        <div id='left' className='item'>
+                            <h1>Stats</h1>
+                            {pokeData.stats.map(el => {
+                                return(
+                                    <div key={el.stat.name} className='col-1'>
+                                    <p>{el.stat.name}:</p>
+                                    <span>{el.base_stat}</span>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className='item'>
+                            <h1>Abilities</h1>
+                            {pokeData.abilities.map(el => {
+                                return(
+                                    <div key={el.ability.name} className='col-2'>
+                                    <p>{el.ability.name}</p>
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
                 </section>
@@ -56,7 +108,6 @@ const Pokemon = (props) => {
 
     return (
         <div className='info'>
-            <h1>{pokemonName}</h1>
             {ShowData()}
         </div>
     )
