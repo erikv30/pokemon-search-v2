6106304355c3454f4d6030fd7f62e7374a2b030b
Erick Vega <erikvega30@hotmail.com>
2021-03-29 03:16:41 -0600

    gh-pages added

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ axios
 lodash
 react-redux
 redux
-redux-thunk redux-devtools-extension
+redux-thunk
+redux-devtools-extension
 react-paginate
 
-and for hosting gh pages
+and for hosting gh-pages
 
 https://erikv30.github.io/pokemon-search-v2
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
 import Store from './Store';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={Store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
